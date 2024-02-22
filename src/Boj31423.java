6870e616347b25047fe08672d06797952ac48407
DevYJKwon <dydwo2324@gmail.com>
2024-02-22 17:29:45 +0900

    Boj 31423 신촌 통폐합 계획

--- src/Boj31423.java
+++ src/Boj31423.java
@@ -1,26 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Boj31423 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String [] arr = new String[n];
-        for(int i=0; i<n; i++){
-            arr[i] = br.readLine();
+        String [] arr = new String[n+1];
+        int [] next = new int[n+1];
+        int [] last = new int [n+1];
+        for(int i=1; i<=n; i++){
+            arr[i]=br.readLine();
+            next[i]=i;
+            last[i]=i;
         }
-        for(int i=0; i<n-1; i++){
+        int index = 0;
+        for(int i=1; i<=n-1; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken())-1;
-            int to = Integer.parseInt(st.nextToken())-1;
-            StringBuilder sb = new StringBuilder();
-            arr[from] = sb.append(arr[from]).append(arr[to]).toString();
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            next[last[from]] = to;
+            last[from]=last[to];
+            index = from;
         }
-        for(int i=0; i<n; i++){
-            System.out.println(arr[i]);
-            break;
+        StringBuilder sb =new StringBuilder();
+        for(int i=1; i<=n; i++){
+            sb.append(arr[index]);
+            index = next[index];
         }
+        System.out.println(sb);
     }
 }
